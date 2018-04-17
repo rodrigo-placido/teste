d2f703a7292e9d3f18a7350bc1f10a618c63e296
rodrigo-placido <rodrigo.placido@gmail.com>
2018-04-17 15:14:48 -0300

    circle center

--- Arruela/ArruelaView.swift
+++ Arruela/ArruelaView.swift
@@ -25,7 +25,7 @@ class ArruelaView: UIView {
         super.init(frame: frame)
         self.backgroundColor = UIColor.gray
         self.clipsToBounds = true
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 200,y: 200), radius: CGFloat(100), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: false)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 150,y: 150), radius: CGFloat(100), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: false)
         
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circlePath.cgPath

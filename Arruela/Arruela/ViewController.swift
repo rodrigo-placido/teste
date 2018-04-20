697b2e6dd774d8e0ca175bac27111c2080e97fc2
rodrigo-placido <rodrigo.placido@gmail.com>
2018-04-19 23:05:30 -0300

    adjustsarruela

--- Arruela/Arruela/ViewController.swift
+++ Arruela/Arruela/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ArruelaViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let circle = ArruelaView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-        circle.setPosition(position: 75)
+        let circle = ArruelaView(frame: CGRect(x: 50, y: 50, width: 260, height: 260))
+        circle.delegate = self
+        circle.setPosition(position: 0)
+        circle.addThumb(position: 30)
         self.view.addSubview(circle)
         
+    }
+    
+    func getPosition(position: Double) {
+        print(position)
     }
 
     override func didReceiveMemoryWarning() {
--- Arruela/ArruelaView.swift
+++ Arruela/ArruelaView.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
+protocol ArruelaViewDelegate:class {
+    func getPosition(position: Double)
+}
+
 class ArruelaView: UIView {
-    let teste = UIView()
-    let circleCenterX:Float = 150.0
-    let circleCenterY:Float = 150.0
+    let Knob = UIView()
+    
+    var circleCenterX = Float()
+    var circleCenterY = Float()
+    var circleRadius = Float()
+    
+    weak var delegate: ArruelaViewDelegate?
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -19,9 +27,13 @@ class ArruelaView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-       
+        //self.backgroundColor = UIColor.yellow
+        
+        circleCenterX = Float(self.frame.size.width) / 2
+        circleCenterY = Float(self.frame.size.height) / 2
+        circleRadius = 120.0
         self.clipsToBounds = true
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 150,y: 150), radius: CGFloat(100), startAngle: CGFloat(degressToRadians(radians: -100)), endAngle:CGFloat(degressToRadians(radians: -80)), clockwise: false)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: CGFloat(circleCenterX), y:  CGFloat(circleCenterY)), radius: CGFloat(circleRadius), startAngle: CGFloat(degressToRadians(radians: -100)), endAngle:CGFloat(degressToRadians(radians: -80)), clockwise: false)
         
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circlePath.cgPath
@@ -31,26 +43,26 @@ class ArruelaView: UIView {
         //you can change the stroke color
         shapeLayer.strokeColor = UIColor.red.cgColor
         //you can change the line width
-        shapeLayer.lineWidth = 3.0
+        shapeLayer.lineWidth = 4.0
         self.layer.addSublayer(shapeLayer)
         
         let color1 = UIColor(red: 225/255.0, green: 255/255.0, blue: 80/255.0, alpha: 1)
         let color2 = UIColor(red: 0/255.0, green: 234/255.0, blue: 214/255.0, alpha: 1)
-        
+        let rect = CGRect(x: 0.0, y: 0.0, width: self.frame.size.width, height: self.frame.size.height)
         let gradient = CAGradientLayer()
-        gradient.frame = frame
+        gradient.frame = rect
         gradient.colors = [color1.cgColor,
                            color2.cgColor]
         gradient.startPoint = CGPoint(x: 0, y: 1)
         gradient.endPoint = CGPoint(x: 1, y: 0)
         gradient.mask = shapeLayer
-        
+
         self.layer.addSublayer(gradient)
         
-        teste.frame = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0)
-        teste.backgroundColor = UIColor.blue
-        teste.isUserInteractionEnabled = true
-        self.addSubview(teste)
+        Knob.frame = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0)
+        Knob.backgroundColor = UIColor.blue
+        Knob.isUserInteractionEnabled = true
+        self.addSubview(Knob)
         
     }
     
@@ -67,32 +79,41 @@ class ArruelaView: UIView {
     }
     
     func calculateHypotenuse(touchX: Float, touchY: Float) -> Float {
-        let distanceX = touchX - 150
-        let distanceY = 150 - touchY
+        let distanceX = touchX - circleCenterX
+        let distanceY = circleCenterY - touchY
         let b = distanceX*distanceX
         let c = distanceY*distanceY
         let a = sqrt(b + c)
         return a
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let currentPoint = touch.location(in: self)
-            // do something with your currentPoint
-        }
-    }
-    
     private func radiansToDegrees(degrees:Double) -> Double{
         return degrees * 180 / .pi
     }
     
-    func setPosition(position: Double) {
+    func addThumb(position: Double) {
         let angle = convertPositionToAngle(position: position)
-        let thumbX = Double((150 + 100 * cos(angle)))
-        let thumbY = Double((150 - 100 * sin(angle)))
+        let thumbX = Double((Double(circleCenterX) + Double(circleRadius) * cos(angle)))
+        let thumbY = Double((Double(circleCenterY) - Double(circleRadius) * sin(angle)))
         let point = CGPoint(x: thumbX, y: thumbY)
         
-        teste.center = point
+        let thumb = UIView()
+        thumb.frame = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0)
+        thumb.backgroundColor = UIColor.gray
+        thumb.isUserInteractionEnabled = true
+        self.addSubview(thumb)
+        thumb.center = point
+        self.bringSubview(toFront: Knob)
+        
+    }
+    
+    func setPosition(position: Double) {
+        let angle = convertPositionToAngle(position: position)
+        let thumbX = Double((Double(circleCenterX) + Double(circleRadius) * cos(angle)))
+        let thumbY = Double((Double(circleCenterY) - Double(circleRadius) * sin(angle)))
+        let point = CGPoint(x: thumbX, y: thumbY)
+        
+        Knob.center = point
     }
     
     private func degressToRadians(radians:Double) -> Double{
@@ -103,48 +124,66 @@ class ArruelaView: UIView {
         let toAngle = (position * 340) / 100
         var normalizedAngle = 0.0
         if (80 > toAngle) {
-            normalizedAngle = 90.0 - toAngle
+            normalizedAngle = 80.0 - toAngle
         } else {
-            normalizedAngle = 360 - abs(toAngle - 90)
+            normalizedAngle = 360 - abs(toAngle - 80)
         }
         return degressToRadians(radians:normalizedAngle)
     }
+    
+    private func distanceBetweensTwoAnglesInClockWise(angle: Double) -> Double {
+        let degrees = radiansToDegrees(degrees: angle)
+        var distance = Double()
+        if (degrees > 80) {
+            distance = 340.0 - (degrees - 80.0)
+        } else {
+            distance = 80.0 - degrees
+        }
+        return distance
+    }
 
-   
+    private func calculatePosition(degress: Double) -> Double {
+        let distance = self.distanceBetweensTwoAnglesInClockWise(angle: degress)
+        return ((distance * 100) / 340)
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let currentPoint = touch.location(in: self)
             let angle = calculateAngle(touchX: Float(currentPoint.x), touchY: Float(currentPoint.y))
             // do something with your currentPoint
             
-            let thumbX = Double((150 + 100 * cos(angle)))
-            let thumbY = Double((150 - 100 * sin(angle)))
+            let thumbX = Double((circleCenterX + circleRadius * cos(angle)))
+            let thumbY = Double((circleCenterY - circleRadius * sin(angle)))
+            
             let point = CGPoint(x: thumbX, y: thumbY)
             
             let degrees = self.radiansToDegrees(degrees: Double(angle))
             let angleAux = (Int(degrees) + 360) % 360
             if (!(angleAux < 100 && angleAux > 80)) {
-                teste.center = point
+                Knob.center = point
+                delegate?.getPosition(position: self.calculatePosition(degress: degrees))
             }
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let currentPoint = touch.location(in: self)
-            let angle = calculateAngle(touchX: Float(currentPoint.x), touchY: Float(currentPoint.y))
-            // do something with your currentPoint
-            
-            let thumbX = Double((150 + 100 * cos(angle)))
-            let thumbY = Double((150 - 100 * sin(angle)))
-            let point = CGPoint(x: thumbX, y: thumbY)
-            
-            let degrees = self.radiansToDegrees(degrees: Double(angle))
-            let angleAux = (Int(degrees) + 360) % 360
-            if (!(angleAux < 100 && angleAux > 80)) {
-                teste.center = point
-            }
-        }
-    }
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let touch = touches.first {
+//            let currentPoint = touch.location(in: self)
+//            let angle = calculateAngle(touchX: Float(currentPoint.x), touchY: Float(currentPoint.y))
+//            // do something with your currentPoint
+//
+//            let thumbX = Double((150 + 100 * cos(angle)))
+//            let thumbY = Double((150 - 100 * sin(angle)))
+//            let point = CGPoint(x: thumbX, y: thumbY)
+//
+//            let degrees = self.radiansToDegrees(degrees: Double(angle))
+//            let angleAux = (Int(degrees) + 360) % 360
+//            if (!(angleAux < 100 && angleAux > 80)) {
+//                Knob.center = point
+//                delegate?.getPosition(position: self.calculatePosition(degress: degrees))
+//            }
+//        }
+//    }
 
 }
